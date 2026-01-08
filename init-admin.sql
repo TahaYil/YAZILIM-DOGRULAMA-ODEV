83508c4f2ee0e95c7425643d2d0927027691607a
TahaYil <taha.yldrms1@gmail.com>
2026-01-08 14:17:42 +0300

    kullanıcıyı her zaman ekleme

--- init-admin.sql
+++ init-admin.sql
@@ -1,8 +1,8 @@
--- init-admin.sql: Docker ile başlatıldığında admin kullanıcı ekler
+
 
 INSERT INTO users (email, password, gender, role)
 VALUES ('admin@admin.com',
-        '$2a$10$wH8Qw8Qw8Qw8Qw8Qw8Qw8OQw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8', -- 'admin' bcrypt hash örneği
+        '$2a$10$wH8Qw8Qw8Qw8Qw8Qw8Qw8OQw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8', 
         'MALE',
         'ADMIN')
 ON CONFLICT (email) DO NOTHING;
