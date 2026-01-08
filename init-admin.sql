124e9edca828d6ebd03c1b7ae44123f01db9b8bb
TahaYil <taha.yldrms1@gmail.com>
2026-01-08 14:28:06 +0300

    kullanıcıyı her zaman ekleme

--- init-admin.sql
+++ init-admin.sql
@@ -1,8 +1,8 @@
-
-
 INSERT INTO users (email, password, gender, role)
-VALUES ('admin@admin.com',
-        '$2a$10$wH8Qw8Qw8Qw8Qw8Qw8Qw8OQw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8', 
-        'MALE',
-        'ADMIN')
+VALUES (
+    'admin@admin.com',
+    '$2a$10$wH8Qw8Qw8Qw8Qw8Qw8Qw8OQw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8',
+    'MALE',
+    'ADMIN'
+)
 ON CONFLICT (email) DO NOTHING;
